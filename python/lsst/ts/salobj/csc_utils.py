7ccc4a033475093c1100b9b8a52a34d9f2a70602
Russell Owen <rowen@uw.edu>
2019-03-12 15:35:11 -0700

    Add set_summary_state function

--- python/lsst/ts/salobj/csc_utils.py
+++ python/lsst/ts/salobj/csc_utils.py
@@ -19,12 +19,111 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ["enable_csc"]
+__all__ = ["enable_csc", "set_summary_state"]
 
 import asyncio
 import re
 
 from lsst.ts import salobj
+from .base_csc import State
+
+
+class _SummaryStateSetter:
+    """Functor to put a CSC into a specified summary state.
+    """
+    def __init__(self):
+        # state index provides an ordering from OFFLINE to ENABLED
+        # (the STATE enums also have integer value, but not in a useful order)
+        self._state_index_dict = {
+            State.OFFLINE: 0,
+            State.STANDBY: 1,
+            State.DISABLED: 2,
+            State.ENABLED: 3,
+        }
+        # state transitions for non-fault states:
+        # keys are (from state index, to state index)
+        # values are state transition command names
+        self._index_cmd_dict = {
+            (0, 1): "enterControl",
+            (1, 2): "start",
+            (2, 3): "enable",
+            (3, 2): "disable",
+            (2, 1): "standby",
+            (1, 0): "exitControl",
+        }
+
+    async def __call__(self, remote, state, settingsToApply="", timeout=1):
+        """Put a CSC into the specified summary state.
+
+        Parameters
+        ----------
+        remote : `Remote`
+            Remote for the CSC to be enabled
+        state : `State` or `int`
+            Desired summary state.
+        settingsToApply : `str`
+            SettingsToApply argument for the ``start`` command.
+            Ignored unless the CSC has to be taken from state
+            `State.STANDBY` to `STATE.DISABLED`.
+        timeout : `float`
+            Timeout for each state transition command and a possible initial
+            summaryState read (sec).
+
+        Notes
+        -----
+        This function assumes the CSC is listening to SAL commands. If the CSC
+        is not running then this function will time out (unless the last
+        reported summary state has been cached and matches ``state``.
+        """
+        state = State(state)
+        if state == State.FAULT:
+            raise ValueError("Cannot go into FAULT state using state transition commands")
+
+        # get current summary state
+        state_data = remote.evt_summaryState.get()
+        if state_data is None:
+            # get failed; try waiting for it, in case the CSC is starting up
+            try:
+                state_data = await remote.evt_summaryState.next(flush=False, timeout=timeout)
+            except asyncio.TimeoutError:
+                raise RuntimeError(f"Cannot get summaryState from {remote.salinfo.name}")
+        current_state = State(state_data.summaryState)
+
+        if current_state == state:
+            # we are already in the desired state
+            return
+        elif current_state == State.FAULT:
+            # first go into standby, then use self._index_cmd_dict
+            await remote.cmd_standby.start(timeout=timeout)
+            current_state = State.STANDBY
+            if current_state == state:
+                return
+
+        old_settings_to_apply = remote.cmd_start.data.settingsToApply
+        try:
+            remote.cmd_start.set(settingsToApply=settingsToApply)
+
+            current_ind = self._state_index_dict[current_state]
+            desired_ind = self._state_index_dict[state]
+            cmdnames = []
+            if desired_ind > current_ind:
+                for ind in range(current_ind, desired_ind):
+                    from_to_ind = (ind, ind+1)
+                    cmdnames.append(self._index_cmd_dict[from_to_ind])
+            elif current_ind > desired_ind:
+                for ind in range(current_ind, desired_ind, -1):
+                    from_to_ind = (ind, ind-1)
+                    cmd = self._index_cmd_dict[from_to_ind]
+                    cmdnames.append(self._index_cmd_dict[from_to_ind])
+
+            for cmdname in cmdnames:
+                cmd = getattr(remote, f"cmd_{cmdname}")
+                await cmd.start(timeout=timeout)
+        finally:
+            remote.cmd_start.data.settingsToApply = old_settings_to_apply
+
+
+set_summary_state = _SummaryStateSetter()
 
 
 def _state_from_ack_error(result):
@@ -49,7 +148,7 @@ async def enable_csc(remote, settingsToApply="", force_config=False, timeout=1):
     force_config : `bool`
         If true then go to `State.STANDBY` state, then `State.ENABLED` state;
         otherwise take the shortest path to `State.ENABLED` state.
-    timeut : `float`
+    timeout : `float`
         Timeout for each state transition command and a possible initial
         summaryState read (sec).
 
--- tests/test_csc_utils.py
+++ tests/test_csc_utils.py
@@ -7,6 +7,8 @@ except ImportError:
     SALPY_Test = None
 from lsst.ts import salobj
 
+STD_TIMEOUT = 2  # timeout for fast operations (sec)
+
 index_gen = salobj.index_generator()
 
 
@@ -17,13 +19,72 @@ class Harness:
         self.csc = salobj.TestCsc(index=index, initial_state=initial_state)
         self.remote = salobj.Remote(SALPY_Test, index)
 
-    async def next_state(self, timeout=1):
+    async def next_state(self, timeout=STD_TIMEOUT):
         state = await self.remote.evt_summaryState.next(flush=False, timeout=timeout)
         return state.summaryState
 
 
+class SetSummaryStateTestCSe(unittest.TestCase):
+    async def assert_state(self, harness, state, timeout=STD_TIMEOUT):
+        actual_state = await harness.next_state(timeout=timeout)
+        self.assertEqual(actual_state, actual_state)
+
+    def test_transitions(self):
+        """Test transitions between all states.
+        """
+        async def doit():
+            settingsToApply = "foo"  # an arbitrary value for testing
+            for initial_state in salobj.State:
+                if initial_state == salobj.State.OFFLINE:
+                    continue
+                    # the Test CSC cannot start in OFFLINE state
+                for final_state in salobj.State:
+                    if final_state == salobj.State.FAULT:
+                        continue
+                        # The function cannot go into FAULT state
+                    with self.subTest(initial_state=initial_state, final_state=final_state):
+                        print(f"initial_state={initial_state!r}; final_state={final_state!r}")
+                        harness = Harness(initial_state=initial_state)
+                        # wait for the initial state (meaning harness is ready)
+                        await harness.remote.evt_summaryState.next(flush=False, timeout=10)
+                        await salobj.set_summary_state(remote=harness.remote, state=final_state,
+                                                       settingsToApply=settingsToApply, timeout=STD_TIMEOUT)
+                        self.assertEqual(harness.csc.summary_state, final_state)
+                        if initial_state in (salobj.State.FAULT, salobj.State.STANDBY) \
+                                and final_state in (salobj.State.DISABLED, salobj.State.ENABLED):
+                            # the start command should have been sent
+                            self.assertEqual(harness.csc.settingsToApply, settingsToApply)
+                        else:
+                            self.assertIsNone(harness.csc.settingsToApply)
+                        # TODO DM-18491 the following should work
+                        # but reliably fails for some cases:
+                        # await asyncio.sleep(0.1)
+                        # state = harness.remote.evt_summaryState.get()
+                        # self.assertEqual(state.summaryState, final_state)
+
+        asyncio.get_event_loop().run_until_complete(doit())
+
+    def test_invalid_state(self):
+        """Test transition to an invalid state."""
+        async def doit():
+            for initial_state in salobj.State:
+                if initial_state == salobj.State.OFFLINE:
+                    continue
+                    # the Test CSC cannot start in OFFLINE state
+                print(f"initial_state={initial_state!r}")
+                harness = Harness(initial_state=initial_state)
+                # wait for the initial state (meaning the harness is ready)
+                await harness.remote.evt_summaryState.next(flush=False, timeout=10)
+                for bad_final_state in (min(salobj.State) - 1, salobj.State.FAULT, max(salobj.State) + 1):
+                    with self.assertRaises(ValueError):
+                        await salobj.set_summary_state(remote=harness.remote, state=bad_final_state,
+                                                       timeout=STD_TIMEOUT)
+
+        asyncio.get_event_loop().run_until_complete(doit())
+
+
 class EnableCscTestCase(unittest.TestCase):
-    async def assert_state(self, harness, state, timeout=1):
+    async def assert_state(self, harness, state, timeout=STD_TIMEOUT):
         actual_state = await harness.next_state(timeout=timeout)
         self.assertEqual(actual_state, actual_state)
 
