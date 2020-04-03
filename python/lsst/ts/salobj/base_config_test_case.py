46f6eb5f5307b25c9cfae953a2ed67419bde6994
Russell Owen <rowen@uw.edu>
2020-04-03 15:03:41 -0700

    Fix "Developed for" boilerplate

--- python/lsst/ts/salobj/base_config_test_case.py
+++ python/lsst/ts/salobj/base_config_test_case.py
@@ -1,6 +1,6 @@
 # This file is part of ts_salobj.
 #
-# Developed for the LSST Data Management System.
+# Developed for the LSST Telescope and Site Systems.
 # This product includes software developed by the LSST Project
 # (https://www.lsst.org).
 # See the COPYRIGHT file at the top-level directory of this distribution
