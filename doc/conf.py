12a615c7911a72edfa468065114610c448e43ef9
Russell Owen <rowen@uw.edu>
2022-04-05 17:48:32 -0700

    doc/conf.py: make linters happier

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@
 This configuration only affects single-package Sphinx documentation builds.
 """
 
-from documenteer.conf.pipelinespkg import *  # noqa
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 import lsst.ts.salobj  # noqa
 
 project = "ts_salobj"
