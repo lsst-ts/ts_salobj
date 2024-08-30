52c1c3b35c01927ec27741a40db24e0418ffea35
Tiago Ribeiro <tribeiro@lsst.org>
2024-08-30 13:36:04 -0700

    Remove idl_metadata module.

--- python/lsst/ts/salobj/__init__.py
+++ python/lsst/ts/salobj/__init__.py
@@ -21,7 +21,6 @@ from .csc_commander import *
 from .csc_utils import *
 from .domain import *
 from .hierarchical_update import *
-from .idl_metadata import *
 from .make_mock_write_topics import *
 from .remote import *
 from .sal_enums import *
