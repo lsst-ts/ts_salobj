d4c1c19c07c08f690572a908fdab490fc82d24e3
Tiago Ribeiro <tribeiro@lsst.org>
2025-01-23 13:37:05 -0700

    fixup! In sal_info.py, update _read_loop to keep track of the last message send timestamp for each topic and discard messages that are delivered out of order.

--- python/lsst/ts/salobj/sal_info.py
+++ python/lsst/ts/salobj/sal_info.py
@@ -1178,8 +1178,8 @@ class SalInfo:
         if self._consumer is None:
             self.log.error("No consumer; quitting")
             return
-        last_sample_timestamps = dict(
-            [(kafka_name, 0.0) for kafka_name in self._read_topics]
+        last_sample_timestamps: dict[str, dict[int, float]] = dict(
+            [(kafka_name, dict()) for kafka_name in self._read_topics]
         )
 
         try:
@@ -1259,17 +1259,23 @@ class SalInfo:
                     schema_resolution_errors[kafka_name] += 1
                     continue
 
-                if data_dict["private_sndStamp"] < last_sample_timestamps[kafka_name]:
+                index = data_dict.get("salIndex", 0)
+                last_sample_timestamp = last_sample_timestamps[kafka_name].get(
+                    index, 0.0
+                )
+
+                if data_dict["private_sndStamp"] < last_sample_timestamp:
                     delay = (
-                        last_sample_timestamps[kafka_name]
-                        - data_dict["private_sndStamp"]
+                        last_sample_timestamp - data_dict["private_sndStamp"]
                     ) * 1000
                     self.log.warning(
                         "Ignoring old topic sample. "
-                        f"Topic sent {delay:0.2f}ms before last sample of {kafka_name}."
+                        f"Topic sent {delay:0.2f}ms before last sample of {kafka_name}:{index}."
                     )
                     continue
-                last_sample_timestamps[kafka_name] = data_dict["private_sndStamp"]
+                last_sample_timestamps[kafka_name][index] = data_dict[
+                    "private_sndStamp"
+                ]
                 data_dict["private_rcvStamp"] = utils.current_tai()
                 data = read_topic.DataType(**data_dict)
 
