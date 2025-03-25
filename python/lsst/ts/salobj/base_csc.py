d63d76e92802bf88412ad05552bd1331fdb2ac4a
Tiago Ribeiro <tribeiro@lsst.org>
2025-03-24 18:46:34 -0700

    In base_csc.py, update check_for_duplicate_heartbeat to create its own Domain and Remote when checking for duplicated CSCs.
    This improves resource cleanup and prevent consumers from dangling around.

--- python/lsst/ts/salobj/base_csc.py
+++ python/lsst/ts/salobj/base_csc.py
@@ -38,8 +38,8 @@ from lsst.ts.xml.sal_enums import State
 from . import base
 from .controller import Controller
 from .csc_utils import make_state_transition_dict
-from .sal_info import SalInfo
-from .topics.remote_event import RemoteEvent
+from .domain import Domain
+from .remote import Remote
 
 HEARTBEAT_INTERVAL = 1  # seconds
 
@@ -271,16 +271,16 @@ class BaseCsc(Controller):
         # to the heartbeat topic for a short time.
         if num_messages < 1:
             raise ValueError(f"{num_messages=} must be positive")
-        async with SalInfo(
-            domain=self.salinfo.domain, name=self.salinfo.name, index=self.salinfo.index
-        ) as salinfo:
-            heartbeat_topic = RemoteEvent(
-                salinfo=salinfo, name="heartbeat", max_history=0
-            )
-            await salinfo.start()
+        async with Domain() as domain, Remote(
+            domain=domain,
+            name=self.salinfo.name,
+            index=self.salinfo.index,
+            readonly=True,
+            include=["heartbeat"],
+        ) as remote:
             try:
-                data = await heartbeat_topic.next(
-                    flush=False, timeout=self.heartbeat_interval * 3
+                data = await remote.evt_heartbeat.next(  # type: ignore[attr-defined]
+                    flush=True, timeout=self.heartbeat_interval * 3
                 )
             except asyncio.TimeoutError:
                 return 0
