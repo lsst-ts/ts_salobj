053530cc72ae562a6434638b443a266ea2166dbc
Eric Coughlin <ecoughlin@lsst.org>
2025-09-26 08:27:58 -0300

    Set local_scheme to no-local-version.

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ import setuptools
 import setuptools_scm
 
 setuptools.setup(
-    version=setuptools_scm.get_version(write_to="python/lsst/ts/salobj/version.py")
+    version=setuptools_scm.get_version(
+        write_to="python/lsst/ts/salobj/version.py", local_scheme="no-local-version"
+    )
 )
